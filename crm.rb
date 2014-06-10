9cb5bbfbee72379408a2b7abf131b2f71223aa31
git-justinpodur <git-justin@podur.org>
2014-06-10 14:39:47 -0400

    got step #4 working

--- crm.rb
+++ crm.rb
@@ -1,20 +1,29 @@
+require_relative 'contact'
 require 'sinatra'
-require relative 'contact'
+require_relative 'rolodex'
+
+# @@rolodex = Rolodex.new
 
 get '/' do
 	@crm_app_name = "Hoostin CRM"
 	erb :index
 end
 
-#View about page
-get '/about' do
-	"The incredible power of about pages is here demonstrated"
-end
-
 #View all contacts
 get '/contacts' do
+	@contacts=[]
+	@contacts << Contact.new("Julie", "Hache", "julie@bitmaerlabs.com", "instructor")
+	@contacts << Contact.new("Justin", "Podur", "justin@podu.org", "student")
+	@contacts << Contact.new("Sample", "Guy", "sample@guy.com", "another one")
+	@contacts << Contact.new("Sample2", "Gal", "sample2@gal.com", "yet another")
 	erb :contacts
 end
+
+# post '/contacts' do
+# 	contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+# 	@@rolodex.add_contact[contact]
+# end
+
 
 #Add new contact
 get '/contacts/new' do
---
+++ rolodex.rb
@@ -0,0 +1,54 @@
+	
+class Rolodex
+		
+	def initialize
+		@contacts = []
+		@id = 1000
+	end
+		
+	attr_reader :contacts
+
+	def add_contact(contact)
+		contact.id=@id
+		@contacts << contact
+		@id +=1
+	end
+
+	def return_contact
+		return @contacts
+	end
+
+	def delete_contact(selected_id)
+		contact_index = @contacts.index(find_contact(selected_id))
+		@contacts.delete_at(contact_index)
+	end
+
+	def find_contact(selected_id)
+		@contacts.find {|contact| contact.id == selected_id}
+	end
+
+	def display_attribute(attribute,id)
+		contact_index = @contacts.index(find_contact(id))
+		if !contact_index
+			puts "Contact not found"
+		else
+			case attribute
+				when 1 then puts "#{@contacts[contact_index].first_name}"
+				when 2 then puts "#{@contacts[contact_index].last_name}"
+				when 3 then puts "#{@contacts[contact_index].email}"
+				when 4 then puts "#{@contacts[contact_index].note}"
+			end
+		end
+	end
+
+
+	def edit_contact(id, new_contact)
+		contact_index = @contacts.index(find_contact(id))
+		if contact_index==nil
+			puts "you have no contacts yet"
+		else
+			@contacts[contact_index]=new_contact
+		end
+	end
+
+end
