7db2d12b4c3e37b1bb7c819b01ab2260f19742ea
git-justinpodur <git-justin@podur.org>
2014-06-10 11:44:50 -0400

    route to /contacts

--- crm.rb
+++ crm.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
 
 get '/' do
-	"Main Menu"
+	@crm_app_name = "Hoostin CRM"
+	erb :index
+end
+
+get '/about' do
+	"The incredible power of about pages is here demonstrated"
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+
+get '/:name' do
+	puts params
+	name = params[:name].capitalize
+	"Hi I'm #{name}!"
 end
