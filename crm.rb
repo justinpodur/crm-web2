dea0cc5a33936a14737fe187d95337eead6dd8c6
git-justinpodur <git-justin@podur.org>
2014-06-12 15:17:16 -0400

    moved contact class into crm.web

--- crm.rb
+++ crm.rb
@@ -1,10 +1,20 @@
-require_relative 'contact'
 require_relative 'rolodex'
 
 require 'sinatra'
 require 'data_mapper'
 
 DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+class Contact
+  attr_accessor :id, :first_name, :last_name, :email, :note
+
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+  end
+end
 
 @@rolodex = Rolodex.new
 
